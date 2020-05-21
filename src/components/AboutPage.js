eaf48f9801b96ed77cae55e638fb2ae3fe490db3
swapnil <swapnil.kalbande@thoughtworks.com>
2020-05-21 12:10:19 +0530

    Added components using function and class with simple routing

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AbourtPage extends React.Component {
+  render() {
+    return (
+      <>
+        {" "}
+        <h2> About </h2>
+        <p>This application in made in React </p>{" "}
+      </>
+    );
+  }
+}
+
+export default AbourtPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1> Home Page </h1>
+      <p>Creating the First componenet for the react page </p>
+      <a href="/about"> About Page</a>
+    </div>
+  );
+}
+
+export default HomePage;
